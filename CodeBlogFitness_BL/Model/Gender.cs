51db7a4d1901111ace3667d8303731fe362adf1b
Oleg Sokolov <oscar.sokolov@gmail.com>
2020-10-05 15:59:17 +0300

    Build Model - User - Gender

---
+++ CodeBlogFitness_BL/Model/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness_BL.Model
+{   /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name of.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Create new Gender.
+        /// </summary>
+        /// <param name="name">Name of gender.</param>
+        public Gender(string name)
+        {
+
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Must have gender", nameof(name));
+            }
+
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+
+        }
+    }
+}
---
+++ CodeBlogFitness_BL/Model/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace CodeBlogFitness_BL.Model
+{/// <summary>
+/// User
+/// </summary>
+    public class User
+    {
+        #region Members
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gender
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// BirthDay
+        /// </summary>
+        public DateTime BirthDay { get; }
+        /// <summary>
+        /// Weight
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Height
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Create new user
+        /// </summary>
+        /// <param name="name">user name</param>
+        /// <param name="gender">user gender</param>
+        /// <param name="birthDay"> user birth day</param>
+        /// <param name="weight">user weight</param>
+        /// <param name="height">user height</param>
+
+        public User(string name, Gender gender, DateTime birthDay, double weight, double height)
+        {
+            #region check the input from the user
+            if (string.IsNullOrWhiteSpace(name))
+            {
+     
+                throw new ArgumentNullException("Name can not be empty.", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Gender can not be empty.", nameof(gender));
+            }
+
+            if (birthDay < DateTime.Parse("01.01.1900") || birthDay > DateTime.Now)
+            {
+                throw new ArgumentException("Birth day is not correct", nameof(birthDay));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Weight is not correct", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Height is not correct", nameof(height));
+            }
+            #endregion
+
+            //
+            Name = name;
+            Gender = gender;
+            BirthDay = birthDay;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
